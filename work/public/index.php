6600fcc91693b3cb287e04c131bd510f77229944
bduha184 <bduha_184@yahoo.co.jp>
2023-04-01 21:32:38 +0900

    戦闘の基本の流れを作成

--- work/public/index.php
+++ work/public/index.php
@@ -1,4 +1,32 @@
 <?php
 
+require_once(__DIR__ . '/../app/Human.php');
+require_once(__DIR__ . '/../app/Enemy.php');
+
 echo "処理のはじまりはじまり～！\n\n";
 
+$tiida = new Human();
+$goblin = new Enemy();
+
+$tiida->name = "ティーダ";
+$goblin->name = "ゴブリン";
+$turn = 1;
+
+while ($tiida->hitPoint > 0 && $goblin->hitPoint > 0) {
+    echo "*** $turn ターン目 ***\n\n";
+    echo $tiida->name . ':' . $tiida->hitPoint . '/' . $tiida::MAX_HITPOINT . "\n";
+    echo $goblin->name . ':' . $goblin->hitPoint . '/' . $goblin::MAX_HITPOINT . "\n";
+
+    echo "\n";
+
+    $tiida->doAttack($goblin);
+    echo "\n";
+    $goblin->doAttack($tiida);
+    echo "\n";
+
+    $turn++;
+}
+
+echo "★★★ 戦闘終了 ★★★\n\n";
+echo $tiida->name . ":" . $tiida->hitPoint . "/" . $tiida::MAX_HITPOINT . "\n";
+echo $goblin->name . ":" . $goblin->hitPoint . "/" . $goblin::MAX_HITPOINT . "\n\n";
